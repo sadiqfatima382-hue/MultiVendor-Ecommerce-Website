beca3ed0cc4925b6a2b7f9856b8abd0d34972759
sadiqfatima382-hue <sadiqfatima382@gmail.com>
2026-07-30 17:55:11 +0500

    feat(validate): enhance validation middleware to include params and query in schema parsing

--- backend/src/middlewares/validate.middleware.js
+++ backend/src/middlewares/validate.middleware.js
@@ -1,7 +1,27 @@
+// export const validate = (schema) => {
+//   return async (req, res, next) => {
+//     try {
+//       req.validatedData = await schema.parseAsync(req.body);
+//       next();
+//     } catch (error) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "Validation failed",
+//         errors: error.issues,
+//       });
+//     }
+//   };
+// };
+
 export const validate = (schema) => {
   return async (req, res, next) => {
     try {
-      req.validatedData = await schema.parseAsync(req.body);
+      req.validatedData = await schema.parseAsync({
+        body: req.body,
+        params: req.params,
+        query: req.query,
+      });
+
       next();
     } catch (error) {
       return res.status(400).json({
