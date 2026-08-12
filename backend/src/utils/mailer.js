3610bfe26a337e846016031cdfdd12d52ac3e7ce
sadiqfatima382-hue <sadiqfatima382@gmail.com>
2026-08-12 15:06:44 +0500

    feat(mailer): add functions for sending OTP, password reset, and order confirmation emails

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,6 +1,9 @@
 import transporter from "../config/mail.js";
+import {  otpEmailTemplate,} from "../templates/emails/otp.template.js";
+import {  passwordResetEmailTemplate,} from "../templates/emails/passwordReset.template.js";
+import {  orderConfirmationEmailTemplate,} from "../templates/emails/orderConfirmation.template.js";
 
-export async function sendEmail({
+async function sendEmail({
   to,
   subject,
   text,
@@ -12,5 +15,58 @@ export async function sendEmail({
     subject,
     text,
     html,
+  });
+}
+
+export async function sendOtpEmail({
+  to,
+  name,
+  otp,
+}) {
+  const email =
+    otpEmailTemplate({
+      name,
+      otp,
+    });
+
+  return sendEmail({
+    to,
+    ...email,
+  });
+}
+
+export async function sendPasswordResetEmail({
+  to,
+  name,
+  resetUrl,
+}) {
+  const email =
+    passwordResetEmailTemplate({
+      name,
+      resetUrl,
+    });
+
+  return sendEmail({
+    to,
+    ...email,
+  });
+}
+
+export async function sendOrderConfirmationEmail({
+  to,
+  name,
+  orderNumber,
+  grandTotal,
+}) {
+  const email =
+    orderConfirmationEmailTemplate({
+      name,
+      orderNumber,
+      grandTotal,
+    });
+
+  return sendEmail({
+    to,
+    ...email,
   });
 }
