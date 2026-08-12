0aa3e4082f795292d71b256ae2b314309bab3b8a
sadiqfatima382-hue <sadiqfatima382@gmail.com>
2026-08-12 17:37:19 +0500

    feat(email-queue): implement email job addition to the queue

---
+++ backend/src/queues/email.queue.js
@@ -0,0 +1,28 @@
+import { emailQueue } from "../config/queue.js";
+
+export async function addEmailJob({
+  to,
+  type,
+  data,
+}) {
+  return emailQueue.add(
+    type,
+    {
+      to,
+      type,
+      data,
+    },
+    {
+      attempts: 3,
+
+      backoff: {
+        type: "exponential",
+        delay: 5000,
+      },
+
+      removeOnComplete: 100,
+
+      removeOnFail: 500,
+    }
+  );
+}
