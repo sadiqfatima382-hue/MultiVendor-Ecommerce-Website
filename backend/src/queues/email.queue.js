2e346263f68ccb6d3b5766764e9473677c561350
sadiqfatima382-hue <sadiqfatima382@gmail.com>
2026-08-17 17:32:15 +0500

    feat(email): implement addEmailJob function for email queue processing

--- backend/src/queues/email.queue.js
+++ backend/src/queues/email.queue.js
@@ -1,32 +1,5 @@
 import {Queue } from "bullmq";
 
-// export async function addEmailJob({
-//   to,
-//   type,
-//   data,
-// }) {
-//   return emailQueue.add(
-//     type,
-//     {
-//       to,
-//       type,
-//       data,
-//     },
-//     {
-//       attempts: 3,
-
-//       backoff: {
-//         type: "exponential",
-//         delay: 5000,
-//       },
-
-//       removeOnComplete: 100,
-
-//       removeOnFail: 500,
-//     }
-//   );
-// }
-
 const emailQueue = new Queue("email-queue", {
   connection: {
     host: process.env.REDIS_HOST || "localhost",
